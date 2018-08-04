ea1d80affdd942b534c62fe1c37267078531d26d
ppoply <34216657+ppoply@users.noreply.github.com>
2018-08-04 19:29:38 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAkUloeiyFCaHxzWtbKxiTpZmO4vdXQp2E';
 
-YTSearch({ key: API_KEY, term: 'surfboards'}, function(data){
-	console.log(data);
-});
 
-// Create a new component. This should produce some HTML
+class App extends Component {
 
-const App = () => {
-	return (
+	constructor(props){
+		super(props);
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 
-	<div>
-		<SearchBar />
-	</div>
+		YTSearch({ key: API_KEY, term: 'surfboards'}, videos => {
+			this.setState({ 
+				videos:videos,
+				selectedVideo: videos[0] 
+			});
+		});
+	}
 
-	);
+	render() {
+		return (
+
+		<div>
+			<SearchBar />
+			<VideoDetail video ={this.state.selectedVideo} />
+			<VideoList 
+
+				onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+				videos = {this.state.videos} />
+		</div>
+		
+		);
+	}
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
 
